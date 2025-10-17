9828b9dca18083c7c91cec461de10247472a95f8
Arnaldo tomo <arnaldotomo@gmail.com>
2025-10-17 15:35:43 +0200

    Update PaymentRequest.php

--- app/Models/PaymentRequest.php
+++ app/Models/PaymentRequest.php
@@ -221,6 +221,7 @@ class PaymentRequest extends Model
         $this->completeShipmentChecklist();
     }
 
+
     // ========================================
     // HELPERS
     // ========================================
